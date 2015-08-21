f1ecc580143c0f0098ec41aec274563672294d4d
bau5 <bau5bot@gmail.com>
2015-08-21 12:22:48 -0700

    Major rework of logic for MP compatibility

--- src/main/java/com/bau5/everyweeks/shadowstep/ItemCatalyst.java
+++ src/main/java/com/bau5/everyweeks/shadowstep/ItemCatalyst.java
@@ -3,6 +3,7 @@ package com.bau5.everyweeks.shadowstep;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.*;
 import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionEffect;
@@ -19,7 +20,6 @@ public class ItemCatalyst extends Item {
     private final double maxDistance = 25.0D;
     private final int maxScalar = 50;
 
-    private int lastInUseDuration = 0;
     private int repairCounter = 0;
     private BlockPos lastBlockPos = null;
 
@@ -35,49 +35,52 @@ public class ItemCatalyst extends Item {
         if (entityIn instanceof EntityPlayer) {
             EntityPlayer player = ((EntityPlayer) entityIn);
 
-            ItemStack stackInUse = player.getItemInUse();
-            if (stackInUse != null && stackInUse.getItem().equals(this)) {
-                lastInUseDuration = player.getItemInUseDuration();
+            if(worldIn.isRemote) {
+                ItemStack stackInUse = player.getItemInUse();
+                if (stackInUse != null && stackInUse.getItem().equals(this)) {
+                    MovingObjectPosition mop = getMovingObjectPositionFromPlayerWithDuration(worldIn, player,
+                            player.getItemInUseDuration(), false);
 
-                MovingObjectPosition mop = getLocationLookingAt(worldIn, player,
-                        getScaledDistance(lastInUseDuration));
-                if (mop != null) {
-                    BlockPos pos = mop.getBlockPos();
-                    if (lastBlockPos == null || lastBlockPos.compareTo(pos) != 0) {
-                        player.playSound("mob.endermen.portal", 1.0F, 0.5F);
-                    }
+                    if (mop != null) {
+                        BlockPos pos = mop.getBlockPos();
+                        if (lastBlockPos == null || lastBlockPos.compareTo(pos) != 0) {
+                            player.playSound("mob.endermen.portal", 0.5F, 0.5F);
+                        }
 
-                    lastBlockPos = pos;
-                    double x = 0;
-                    double y = 1.5D;
-                    double z = 0;
-                    switch (mop.sideHit) {
-                        case SOUTH:
-                            z += 1.0D;
-                        case NORTH:
-                            x += itemRand.nextDouble();
-                            break;
-                        case EAST:
-                            x += 1.0D;
-                        case WEST:
-                            z += itemRand.nextDouble();
-                            break;
-                        case DOWN:
-                            y -= 1.0D;
-                            x += itemRand.nextDouble();
-                            z += itemRand.nextDouble();
-                            break;
-                        case UP:
-                            y += 0.5D;
-                            x += itemRand.nextDouble();
-                            z += itemRand.nextDouble();
-                            break;
+                        lastBlockPos = pos;
+                        double x = 0;
+                        double y = 1.5D;
+                        double z = 0;
+                        switch (mop.sideHit) {
+                            case SOUTH:
+                                z += 1.0D;
+                            case NORTH:
+                                x += itemRand.nextDouble();
+                                break;
+                            case EAST:
+                                x += 1.0D;
+                            case WEST:
+                                z += itemRand.nextDouble();
+                                break;
+                            case DOWN:
+                                y -= 1.0D;
+                                x += itemRand.nextDouble();
+                                z += itemRand.nextDouble();
+                                break;
+                            case UP:
+                                y += 0.5D;
+                                x += itemRand.nextDouble();
+                                z += itemRand.nextDouble();
+                                break;
+                        }
+                        worldIn.spawnParticle(EnumParticleTypes.PORTAL, pos.getX() + x, (double) pos.getY() + y,
+                                pos.getZ() + z, 0.0D, -2.0D, 0.0D);
                     }
-                    worldIn.spawnParticle(EnumParticleTypes.PORTAL, pos.getX() + x, (double) pos.getY() + y,
-                            pos.getZ() + z, 0.0D, -2.0D, 0.0D);
                 }
             } else {
-                boolean isNight = worldIn.getWorldTime() > 135000 && worldIn.getWorldTime() < 24000;
+                // Repair only needs to happen on Server, syncs the changes to Client. If both do it
+                //client will incorrectly show the repaired amount being twice what it actually is
+                boolean isNight = worldIn.getWorldTime() > 13500 && worldIn.getWorldTime() < 23500;
                 if (stack.isItemDamaged() && isNight) {
                     repairCounter++;
                     if (repairCounter > 225) {
@@ -87,14 +90,16 @@ public class ItemCatalyst extends Item {
                 }
             }
         }
+
         super.onUpdate(stack, worldIn, entityIn, itemSlot, isSelected);
     }
 
     @Override
     public void onPlayerStoppedUsing(ItemStack stack, World worldIn, EntityPlayer playerIn, int timeLeft) {
-        double scaled = getScaledDistance(lastInUseDuration);
-        MovingObjectPosition pos = getLocationLookingAt(worldIn, playerIn,
-                scaled);
+        int ticks = getMaxItemUseDuration(stack) - timeLeft;
+        double scaled = getScaledDistance(ticks);
+
+        MovingObjectPosition pos = getMovingObjectPositionFromPlayerWithDuration(worldIn, playerIn, ticks, false);
         if (pos != null && pos.typeOfHit == MovingObjectPosition.MovingObjectType.BLOCK) {
             // find first open space
             BlockPos hit = pos.getBlockPos();
@@ -103,8 +108,8 @@ public class ItemCatalyst extends Item {
                 if (!isValid(search, worldIn)) {
                     search = search.up();
                 } else {
-                    int damage = (int) Math.ceil((scaled / maxDistance) * damageScalar);
-                    if (stack.getItemDamage() + damage < getMaxDamage()) {
+                    int damage = (int) Math.ceil(Math.sqrt(playerIn.getDistanceSq(hit)) / maxDistance * damageScalar);
+                    if (stack.getItemDamage() + damage < getMaxDamage() || playerIn.capabilities.isCreativeMode) {
                         BlockPos playerPos = playerIn.getPosition();
                         // spawn particles before stepping
                         for (int j = 0; j < 30; j++) {
@@ -123,7 +128,13 @@ public class ItemCatalyst extends Item {
                         // move player, play sound, spawn particles
                         worldIn.playSoundEffect(playerIn.posX, playerIn.posY + 0.5, playerIn.posZ, "mob.endermen.portal",
                                 1.0F, 1.0F);
-                        playerIn.setPositionAndUpdate(x + 0.5, y, z + 0.5);
+
+                        if (playerIn instanceof EntityPlayerMP) {
+                            ((EntityPlayerMP)playerIn).playerNetServerHandler
+                                    .setPlayerLocation(x + 0.5, y, z + 0.5, playerIn.rotationYaw, playerIn.rotationPitch);
+                        } else {
+                            playerIn.setPosition(x + 0.5, y, z + 0.5);
+                        }
 
                         worldIn.playSoundEffect(x, y + 0.5, z, "mob.endermen.portal", 1.0F, 1.0F);
                         for (int j = 0; j < 30; j++) {
@@ -144,11 +155,26 @@ public class ItemCatalyst extends Item {
         return world.isAirBlock(position) && world.isAirBlock(position.add(0, 1, 0));
     }
 
-    public MovingObjectPosition getLocationLookingAt(World world, EntityPlayer player, double distance) {
-        Vec3 eyes = player.getPositionEyes(1.0F);
-        Vec3 look = player.getLookVec();
-        Vec3 end = eyes.addVector(look.xCoord * distance, look.yCoord * distance, look.zCoord * distance);
-        return world.rayTraceBlocks(eyes, end, true, true, false);
+    /**
+     *   Modified version of super.getMovingObjectPositionFromPlayer. Needed to add the scaled
+     * distance, and it's different between client and server, therefore better to do it this way.
+     */
+    private MovingObjectPosition getMovingObjectPositionFromPlayerWithDuration(World worldIn, EntityPlayer playerIn, int duration, boolean useLiquids) {
+        float f = playerIn.prevRotationPitch + (playerIn.rotationPitch - playerIn.prevRotationPitch);
+        float f1 = playerIn.prevRotationYaw + (playerIn.rotationYaw - playerIn.prevRotationYaw);
+        double d0 = playerIn.prevPosX + (playerIn.posX - playerIn.prevPosX);
+        double d1 = playerIn.prevPosY + (playerIn.posY - playerIn.prevPosY) + (double)playerIn.getEyeHeight();
+        double d2 = playerIn.prevPosZ + (playerIn.posZ - playerIn.prevPosZ);
+        Vec3 vec3 = new Vec3(d0, d1, d2);
+        float f2 = MathHelper.cos(-f1 * 0.017453292F - (float)Math.PI);
+        float f3 = MathHelper.sin(-f1 * 0.017453292F - (float)Math.PI);
+        float f4 = -MathHelper.cos(-f * 0.017453292F);
+        float f5 = MathHelper.sin(-f * 0.017453292F);
+        float f6 = f3 * f4;
+        float f7 = f2 * f4;
+        double d3 = getScaledDistance(duration);
+        Vec3 vec31 = vec3.addVector((double)f6 * d3, (double)f5 * d3, (double)f7 * d3);
+        return worldIn.rayTraceBlocks(vec3, vec31, useLiquids, !useLiquids, false);
     }
 
     /**
